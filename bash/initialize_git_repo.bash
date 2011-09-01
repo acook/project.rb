0baed2567ac5a1861f24fb4d80e76f6f2a64b9d5
Anthony Cook <anthonymichaelcook@gmail.com>
2011-09-01 01:31:55 -0700

    Won't need the bash stuff since we have the template now

--- bash/initialize_git_repo.bash
+++ bash/initialize_git_repo.bash
@@ -2,18 +2,17 @@
 
 # check for git exit with error if not found
 
-readme=README.markdown
+#mkdir $1
+#cd $1
 
-mkdir $1
-cd $1
-
-echo $1 > $readme
-echo ====== >> $readme
-echo >> $readme
-echo This is a new repo for the $readme project. >> $readme
-echo >> $readme
+#readme=README.markdown
+#echo $1 > $readme
+#echo ====== >> $readme
+#echo >> $readme
+#echo This is a new repo for the $readme project. >> $readme
+#echo >> $readme
 
 git init
-git add $readme
-git commit -m "first commit for $readme"
+git add .
+git commit -m "first commit for $1"
 
